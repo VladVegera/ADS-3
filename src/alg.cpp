f650802321495de32978638bbd04fe33c2b4d419
VladVegera <94718115+VladVegera@users.noreply.github.com>
2022-03-13 22:48:01 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,12 +1,12 @@
 // Copyright 2021 NNTU-CS
 int cbinsearch(int *arr, int size, int value) {
   int count = 0;
-  for(int i = 0; i < size; i++){
-    if(arr[i] == value) {
+  for (int i = 0; i < size; i++) {
+    if (arr[i] == value) {
       count++;
     }
   }
-  if (count>0) {
+  if (count > 0) {
     return count;
   } else {
     return 0; // если ничего не найдено
